464517545b969a8551ed7d78ae97fdea920c08fc
Peter Holm <peter@l-holm.dk>
2022-04-05 12:45:31 +0200

    pictures

---
+++ config_site/func.php
@@ -0,0 +1,133 @@
+<?php
+
+$windows = 0;
+if (PHP_OS == 'WINNT') {
+    $windows = 1;
+    $config_dir = "config";
+}
+
+# get OS information
+
+function get_hw_info()
+{
+    global $windows;
+    if ($windows) return "na";
+    $cmd = "cat /proc/device-tree/model";
+    unset($output);
+    $r = exec($cmd, $output, $result);
+    return $output[0];
+}
+
+# get networks information
+
+function get_ip_address()
+{
+    return($_SERVER['SERVER_ADDR']);
+}
+
+function get_eth_mac()
+{   
+    global $windows;
+    if ($windows) return("Unknown");
+    $out = exec('ifconfig eth0 | grep ether | tr -s " " | cut -d " " -f 3');
+    return $out;
+}
+
+function get_wifi_mac()
+{
+    $out =  exec('ifconfig wlan0 | grep ether | tr -s " " | cut -d " " -f 3');
+    return($out);
+}
+
+function read_config_file($file, &$ssid, &$passphrase)
+{
+    $content = parse_ini_file($file);
+    $ssid = $content['ssid'];
+    $passphrase = $content['passphrase'];
+    return;
+}
+
+# internet
+function internet_connection()
+{
+    $out = exec('ping -w 500 -n 1 8.8.8.8', $output, $res);
+    #print ("res ".$res);
+    #print $out;
+    if ($res) return "Not OK";
+    return "OK";
+    return $out;
+}
+
+# get wifi information
+
+function get_ap_list()
+{
+    unset($output);
+    $cmd = 'wpa_cli scan_result | cut -f5';
+    $cmd = '/sbin/wpa_cli scan_result  ';
+    $r =    exec($cmd, $output, $result);
+    //echo "Result: $result r: $r\n";
+    //print_r($output);
+    return $output;
+}
+
+function get_wifi_list() 
+{
+    $cmd = 'sudo iwlist wlan0 scan | egrep "Cell|ESSID|Signal|Rates"';
+    $res = exec ($cmd, $output, $result);
+    $out = implode("<br>\n", $output);
+    return $out;
+}
+
+function create_wpa_config($path, $ssid, $passphrase)
+{
+    $config_content = "network={\nssid=\"$ssid\"\npsk=\"$passphrase\"\nkey_mgmt=WPA-PSK\n}\n";
+    if (!file_put_contents($path, $config_content))
+        echo "wpa_file_put_content went wrong";
+}
+
+
+function get_wifi_status()
+{
+    unset($output);
+    $cmd = 'iwconfig wlan0';
+    $r =    exec($cmd, $output, $result);
+    //echo "Result: $result r: $r\n";
+    //print_r($output);
+    return "<pre>".implode("\n",$output)."</pre>"; 
+}
+
+
+
+
+
+//print_r($_SERVER);
+$pl['HW Info'] = get_hw_info();
+$pl['Rasbian'] = exec('grep "VERSION=" /etc/os-release');
+$pl['HostName'] = gethostname();
+$pl["Server Software"] =  $_SERVER['SERVER_SOFTWARE'];
+$pl['Python 2'] = exec('python --version 2>&1');
+$pl['Python 3'] = exec('python3 --version 2>&1');
+$pl["ServerName"] =  $_SERVER['SERVER_NAME'];
+if (!$windows)
+    $pl["Server IP addr"] =  $_SERVER['SERVER_ADDR'];
+$pl["Ether MAC"] =  exec('ifconfig eth0 | grep ether | tr -s " " | cut -d " " -f 3');
+$pl["Wlan MAC"] =  exec('ifconfig wlan0 | grep ether | tr -s " " | cut -d " " -f 3');
+
+$totalmem = exec('grep MemTot /proc/meminfo| tr -s " " |cut -d " " -f 2');
+$freemem = exec('grep MemFree /proc/meminfo| tr -s " " |cut -d " " -f 2');
+unset($output);
+$mem = exec('head -5 /proc/meminfo', $output);
+$str = "";
+foreach ($output as $o) $str .= $o . "<br>";
+unset($output);
+$str2="";
+$mem = exec('df -h / /boot | tail -3',$output);
+foreach ($output as $o) $str2 .= $o . "<br>";
+$pl2["Memory"] = $str;
+$pl2["Total Disk"] = $str2;
+$pl2['Load'] = exec('cat /proc/loadavg');
+$pl2['Batteri Level'] = '<progress id="battery" value="90" max="100">90%</progress>';
+?>
+
+
--- config_site/index.php
+++ config_site/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, xheight=device-height, initial-scale=1.0">
   <title>Danwand Configuration</title>
   <!-- <link rel="manifest" href="/manifest.json" crossorigin="use-credentials"> -->
   <link rel="icon" href="favicon.ico" type="image/x-icon">
@@ -13,9 +13,15 @@
 </head>
 
 <body>
+  <?php
+  require('func.php');
+  ?>
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">DanWand</a>
+    <nav class="navbar navbar-expand-lg navbar-dark xbg-primary">
+      <a class="navbar-brand" href="#">
+        <img src="/pic/db_logo.png" width="30" height="30" class="d-inline-block align-top" alt="logo">
+        danWand
+      </a>&nbsp;
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -46,14 +52,66 @@
         </ul>
       </div>
     </nav>
-
-    <h1>Current configuration</h1>
-
-    Wifi Network
-
-    If config mode
-
-    else wpa mode
+    <div class="background-wand">
+      <h1 class="text-center">danWand configuration</h1>
+      <br>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>WIFI SID</label>
+        </div>
+        <div class="col-4">
+          FutureBox
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>ETH Mac Address</label>
+        </div>
+        <div class="col-4">
+          <?php echo get_eth_mac() ?>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>WiFi Mac Address</label>
+        </div>
+        <div class="col-4">
+          <?= get_wifi_mac() ?>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>IP Address</label>
+        </div>
+        <div class="col-4">
+          <?php echo get_ip_address()?>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>Router</label>
+        </div>
+        <div class="col-4">
+          192.168.1.1
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>Internet</label>
+        </div>
+        <div class="col-4">
+          <?=internet_connection()?>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <label>HW info</label>
+        </div>
+        <div class="col-4">
+          <?=get_hw_info()?>
+        </div>
+      </div>
+   </div>
   </div>
   <script src="/js/bootstrap.min.js"></script>
 
